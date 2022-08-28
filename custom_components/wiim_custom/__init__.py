9aba4ae4616ff464e7d2ebfa866d7398ad3e9e31
onlyoneme <only.one.me.74@gmail.com>
2022-08-29 00:25:15 +0200

    play url

--- custom_components/wiim_custom/__init__.py
+++ custom_components/wiim_custom/__init__.py
@@ -13,14 +13,23 @@ from homeassistant.helpers import config_validation as cv
 DOMAIN = 'wiim_custom'
 
 SERVICE_CMD = 'command'
+SERVICE_PLAY_URL = 'play_url'
+
 
 ATTR_CMD = 'command'
 ATTR_NOTIF = 'notify'
+ATTR_URL = 'url'
+
 
 CMND_SERVICE_SCHEMA = vol.Schema({
     vol.Required(ATTR_ENTITY_ID): cv.comp_entity_ids,
     vol.Required(ATTR_CMD): cv.string,
     vol.Optional(ATTR_NOTIF, default=True): cv.boolean
+})
+
+PLAY_URL_SERVICE_SCHEMA = vol.Schema({
+    vol.Required(ATTR_ENTITY_ID): cv.entity_id,
+    vol.Required(ATTR_URL): cv.template
 })
 
 _LOGGER = logging.getLogger(__name__)
@@ -47,9 +56,17 @@ def setup(hass, config):
                 if device.entity_id in entity_ids:
                     _LOGGER.debug("**COMMAND** entity: %s; command: %s", device.entity_id, command)
                     await device.async_execute_command(command, notify)
+        elif service.service == SERVICE_PLAY_URL:
+            url = service.data.get(ATTR_URL)
+            for device in entities:
+                if device.entity_id in entity_ids:
+                    _LOGGER.debug("**PLAY URL** entity: %s; url: %s", device.entity_id, url)
+                    await device.call_wiim_httpapi("setPlayerCmd:play:{0}".format(url), None)
+
 
     hass.services.async_register(
         DOMAIN, SERVICE_CMD, async_service_handle, schema=CMND_SERVICE_SCHEMA)
-
+    hass.services.async_register(
+        DOMAIN, SERVICE_PLAY_URL, async_service_handle, schema=PLAY_URL_SERVICE_SCHEMA)
 
     return True
